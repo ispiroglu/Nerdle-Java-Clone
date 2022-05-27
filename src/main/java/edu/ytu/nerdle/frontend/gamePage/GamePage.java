c5c5c0335feab9c7a9ad6d6b709cc7c40fe8756d
Evren Ispiroglu <evrenn.ispiroglu@gmail.com>
2022-05-27 12:31:24 +0300

    designing game page

---
+++ src/main/java/edu/ytu/nerdle/frontend/gamePage/GamePage.java
@@ -0,0 +1,65 @@
+package edu.ytu.nerdle.frontend.gamePage;
+
+import javax.swing.*;
+
+public class GamePage {
+    private JPanel gamePanel;
+    private JPanel equationField;
+    private JPanel operationFields;
+    private JPanel equationField1_1;
+    private JPanel equationField1_2;
+    private JPanel equationField1_3;
+    private JPanel equationField1_4;
+    private JPanel equationField1_5;
+    private JPanel equationField1_6;
+    private JPanel equationField1_7;
+    private JPanel equationField1_8;
+    private JPanel equationField1_9;
+    private JPanel equationField2_1;
+    private JPanel equationField2_2;
+    private JPanel equationField2_3;
+    private JPanel equationField2_4;
+    private JPanel equationField2_5;
+    private JPanel equationField2_6;
+    private JPanel equationField2_7;
+    private JPanel equationField2_8;
+    private JPanel equationField2_9;
+    private JPanel equationField3_1;
+    private JPanel equationField3_2;
+    private JPanel equationField3_3;
+    private JPanel equationField3_4;
+    private JPanel equationField3_5;
+    private JPanel equationField3_6;
+    private JPanel equationField3_7;
+    private JPanel equationField3_8;
+    private JPanel equationField3_9;
+    private JPanel equationField4_1;
+    private JPanel equationField4_2;
+    private JPanel equationField4_3;
+    private JPanel equationField4_4;
+    private JPanel equationField4_5;
+    private JPanel equationField4_6;
+    private JPanel equationField4_7;
+    private JPanel equationField4_8;
+    private JPanel equationField4_9;
+    private JPanel equationField5_1;
+    private JPanel equationField5_2;
+    private JPanel equationField5_3;
+    private JPanel equationField5_4;
+    private JPanel equationField5_5;
+    private JPanel equationField5_6;
+    private JPanel equationField5_7;
+    private JPanel equationField5_8;
+    private JPanel equationField5_9;
+    private JPanel equationField6_1;
+    private JPanel equationField6_2;
+    private JPanel equationField6_3;
+    private JPanel equationField6_4;
+    private JPanel equationField6_5;
+    private JPanel equationField6_6;
+    private JPanel equationField6_7;
+    private JPanel equationField6_8;
+    private JPanel equationField_6_9;
+
+
+}
--- src/main/java/edu/ytu/nerdle/frontend/mainPage/MainPage.java
+++ src/main/java/edu/ytu/nerdle/frontend/mainPage/MainPage.java
@@ -1,20 +1,18 @@
 package edu.ytu.nerdle.frontend.mainPage;
 
 import javax.swing.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
-public class mainPage {
+public class MainPage {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("mainPage");
-        frame.setContentPane(new mainPage().panel1);
+        frame.setContentPane(new MainPage().JPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
 
-    private JPanel panel1;
+    private JPanel JPanel;
     private JPanel mainPanel;
     private JPanel statsPanel;
     private JPanel newGamePanel;
@@ -22,16 +20,11 @@ public class mainPage {
     private JPanel testPanel;
     private JLabel gameNameLabel;
     private JPanel gameNamePanel;
-    private JLabel asude;
+    private JButton continueButton;
+    private JButton newGameButton;
+    private JLabel statsLabel;
+    private JButton testButton;
 
-    public mainPage() {
-        statsPanel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-                System.out.println("ASUDE BIR SEYI DE ANLA ARTIK");
-
-            }
-        });
+    public MainPage() {
     }
 }
