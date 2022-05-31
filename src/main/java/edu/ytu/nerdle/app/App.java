c053aef818d5fb518eef97b15be6de9461b4c046
Evren Ispiroglu <evrenn.ispiroglu@gmail.com>
2022-05-31 21:29:39 +0300

    last commit

--- src/main/java/edu/ytu/nerdle/app/App.java
+++ src/main/java/edu/ytu/nerdle/app/App.java
@@ -3,8 +3,11 @@ package edu.ytu.nerdle.app;
 
 import edu.ytu.nerdle.frontend.mainPage.MainPage;
 
+import java.awt.*;
+
 public class App {
     public static void main(String[] args) {
+
         MainPage mainPage = new MainPage();
         mainPage.loadStats();
         mainPage.pack();
